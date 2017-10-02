824276912eadffb1b0303ec0d38722bce40a3220
jagrio <jagrio@iti.gr>
2017-10-02 16:03:53 +0300

    Added perf and bargraph generation

--- MLslippagesrc/MLslippage/test.py
+++ MLslippagesrc/MLslippage/test.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python
 import time
-# start_time = time.time()
-from copy import deepcopy
+from copy import deepcopy, copy
 import math
-# import scipy.io as sio
+import scipy.io as sio
 import shutil
-import os
+import os, errno
 from random import shuffle
 import numpy as np
 from pylab import *
-from featext import feat
+from featext import *
 import matplotlib.pyplot as plt
-#get_ipython().magic(u'matplotlib qt')
-#matplotlib qt
-# inline (suitable for ipython only, shown inside browser!) or qt (suitable in general, shown in external window!)
 from matplotlib.colors import ListedColormap
-from mpl_toolkits.mplot3d import axes3d, Axes3D
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.datasets import make_moons, make_circles, make_classification
-from sklearn.decomposition import PCA, KernelPCA
-from sklearn.manifold import TSNE
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.linear_model import LinearRegression, LogisticRegression, LassoCV, RandomizedLasso
-from sklearn.feature_selection import SelectFromModel, SelectKBest, chi2, f_classif, mutual_info_classif
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
 from sklearn.exceptions import ConvergenceWarning
-from sklearn.metrics import f1_score, roc_auc_score
-from collections import OrderedDict
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, confusion_matrix
 import re
 import datetime
 import urllib
 import tarfile
-import scipy.io as sio
+import zipfile
+import joblib
+from subprocess import call, check_output
+from joblib import Parallel, delayed, Memory
+from tempfile import mkdtemp
+import copy_reg
+import types
+import itertools
+import glob
 
 sys.exit(0)
